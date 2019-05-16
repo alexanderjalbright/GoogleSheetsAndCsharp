38669d37f728ea448509d818f1ef3df319549d9c
Alex Albright <alexanderjalbright@gmail.com>
2019-05-16 15:44:25 -0400

    Reading data successful

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Sheets.v4;
 
 
 namespace GoogleSheetsAndCsharp
@@ -6,10 +9,46 @@ namespace GoogleSheetsAndCsharp
     class Program
     {
         static readonly string[] Scopes = { SheetsService.Scope.Spreadsheets };
-
+        static readonly string ApplicationName = "Legislators";
+        static readonly string SpreadsheetId = "1Z4qmu8ErL6RVq3kqEzTbSp8KZWizJGSpfTO0mRB_qjk"; //from URL
+        static readonly string sheet = "congress";
+        static SheetsService service;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            GoogleCredential credential;
+            using (var stream = new FileStream("client_secrets.json", FileMode.Open, FileAccess.Read))
+            {
+                credential = GoogleCredential.FromStream(stream).CreateScoped(Scopes);
+            }
+
+            service = new SheetsService(new Google.Apis.Services.BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            ReadEntries();
+        }
+
+        static void ReadEntries()
+        {
+            var range = $"{sheet}!A1:F10";
+            var request = service.Spreadsheets.Values.Get(SpreadsheetId, range);
+
+            var response = request.Execute();
+            var values = response.Values;
+            if(values != null && values.Count > 0)
+            {
+                foreach(var row in values)
+                {
+                    Console.WriteLine("{0} {1} | {2} | {3}", row[5], row[4], row[3], row[1]);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No Data found.");
+            }
+
         }
     }
 }

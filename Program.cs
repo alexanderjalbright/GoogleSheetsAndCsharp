104120212afb1579a12f8382378ba0c7a745a163
Alex Albright <alexanderjalbright@gmail.com>
2019-05-16 15:25:46 -0400

    Corrected Google API package issue

--- Program.cs
+++ Program.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 
+
 namespace GoogleSheetsAndCsharp
 {
     class Program
     {
+        static readonly string[] Scopes = { SheetsService.Scope.Spreadsheets };
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
